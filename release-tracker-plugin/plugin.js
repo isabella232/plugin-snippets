aa9338d7952603507fb5357c3c029706fb5848ca
yakkomajuri <yakko.majuri@gmail.com>
2020-12-22 11:11:50 -0300

    Run prettier

--- release-tracker-plugin/plugin.js
+++ release-tracker-plugin/plugin.js
@@ -1,20 +1,21 @@
 async function setupPlugin({ config, global }) {
     global.posthogHost = config.posthogHost.includes('http') ? config.posthogHost : 'https://' + config.posthogHost
 
-    global.posthogOptions =  { 
+    global.posthogOptions = {
         headers: {
-            'Authorization': `Bearer ${config.posthogApiKey}`
-        }
-    } 
+            Authorization: `Bearer ${config.posthogApiKey}`,
+        },
+    }
 
     const ghBasicAuthToken = Buffer.from(`${config.ghOwner}:${config.ghToken}`).toString('base64')
-    
 
-    global.ghOptions = config.ghToken ? { 
-        headers: {
-            'Authorization': `Basic ${ghBasicAuthToken}`
-        }
-    } : {}
+    global.ghOptions = config.ghToken
+        ? {
+              headers: {
+                  Authorization: `Basic ${ghBasicAuthToken}`,
+              },
+          }
+        : {}
 
     try {
         const posthogRes = await fetchWithRetry(`${global.posthogHost}/api/user`, global.posthogOptions)
@@ -30,7 +31,6 @@ async function setupPlugin({ config, global }) {
         if (githubRes.status !== 200) {
             throw new Error('Invalid GitHub repo owner or name')
         }
-
     } catch {
         throw new Error('Invalid PostHog Personal API key')
     }
@@ -38,12 +38,12 @@ async function setupPlugin({ config, global }) {
 
 async function runEveryDay({ config, global }) {
     const annotationsResponse = await fetchWithRetry(
-        `${global.posthogHost}/api/annotation/?scope=organization&deleted=false`, 
+        `${global.posthogHost}/api/annotation/?scope=organization&deleted=false`,
         global.posthogOptions
     )
 
     const annotationsJson = await annotationsResponse.json()
-    let annotations = new Set(annotationsJson.results.map(annotation => annotation.content))
+    let annotations = new Set(annotationsJson.results.map((annotation) => annotation.content))
 
     const ghTagsResponse = await fetchWithRetry(
         `https://api.github.com/repos/${config.ghOwner}/${config.ghRepo}/git/refs/tags`,
@@ -52,54 +52,51 @@ async function runEveryDay({ config, global }) {
     const ghTagsJson = await ghTagsResponse.json()
 
     const newTags = ghTagsJson
-        .map(tag => ({
-            name: tag.ref.split('refs/tags/')[1], 
-            url: tag.object.url
+        .map((tag) => ({
+            name: tag.ref.split('refs/tags/')[1],
+            url: tag.object.url,
         }))
-        .filter(tag => !annotations.has(tag.name))
-    
+        .filter((tag) => !annotations.has(tag.name))
+
     for (let tag of newTags) {
         const tagDetailsResponse = await fetchWithRetry(tag.url, global.ghOptions)
         const tagDetailsJson = await tagDetailsResponse.json()
 
-        const createAnnotationRes = await fetchWithRetry(`${global.posthogHost}/api/annotation/`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${config.posthogApiKey}`
+        const createAnnotationRes = await fetchWithRetry(
+            `${global.posthogHost}/api/annotation/`,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${config.posthogApiKey}`,
+                },
+                body: JSON.stringify({
+                    content: tag.name,
+                    scope: 'organization',
+                    date_marker: getTagDate(tagDetailsJson),
+                }),
             },
-            body: JSON.stringify({
-                "content": tag.name,
-                "scope": "organization",
-                "date_marker": getTagDate(tagDetailsJson)
-            })
-        }, 'POST')
-        
-        if (createAnnotationRes.status === 201) {
-            posthog.capture('created_tag_annotation', {tag: tag.name})
-        }
-    }   
+            'POST'
+        )
 
+        if (createAnnotationRes.status === 201) {
+            posthog.capture('created_tag_annotation', { tag: tag.name })
+        }
+    }
 }
 
-
-async function fetchWithRetry(url, options={}, method='GET', isRetry=false) {
+async function fetchWithRetry(url, options = {}, method = 'GET', isRetry = false) {
     try {
-        const res = await fetch(url, {method: method, ...options})
+        const res = await fetch(url, { method: method, ...options })
         return res
     } catch {
         if (isRetry) {
             throw new Error(`${method} request to ${url} failed.`)
-        } 
-        const res = await fetchWithRetry(url, options, method=method, isRetry=true)
+        }
+        const res = await fetchWithRetry(url, options, (method = method), (isRetry = true))
         return res
     }
 }
 
 function getTagDate(tag) {
-    return (
-        tag.tagger ? tag.tagger.date : 
-        tag.author ? tag.author.date : 
-        tag.committer ? tag.committer.date : 
-        null
-    )
+    return tag.tagger ? tag.tagger.date : tag.author ? tag.author.date : tag.committer ? tag.committer.date : null
 }
